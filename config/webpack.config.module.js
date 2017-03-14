501cfca47cfb8788c33a8e79b48b247dd2602679
samuv <verzi@mailup.com>
2017-03-14 10:09:41 +0100

    add uglify on dist

--- config/webpack.config.module.js
+++ config/webpack.config.module.js
@@ -20,5 +20,18 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
+      }
+    }),
   ]
 }
